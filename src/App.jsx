15bf0fd44eee8d93b5f160f11443f08fcf09fb52
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-05-08 11:28:58 +0200

    feat: add homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import './App.css'
-import {usePeople} from "./reducers/hooks/usePeople.js";
-import {Table} from "./components/table/table.jsx";
-import {Pagination} from "./components/pagination/pagination.jsx";
+import {HomePage} from "./pages/home-page/home-page.jsx";
 
 function App() {
-    const {
-        loading,
-        people,
-        prevPage,
-        nextPage,
-        goNextPage,
-        goPrevPage
-    } = usePeople();
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Star Wars Characters</h1>
-            </header>
-            <main>
-                <Table people={people} loading={loading} prevPage={prevPage} nextPage={nextPage} goNextPage={goNextPage}
-                       goPrevPage={goPrevPage}/>
-                <Pagination hasPrevPage={loading || !prevPage} hasNextPage={loading || !nextPage} goNextPage={goNextPage} goPrevPage={goPrevPage}/>
-            </main>
-        </div>);
+        <HomePage/>)
 }
 
 export default App
