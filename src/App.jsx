a783361b177c9e561d8ff18b27ceda9a54e785ad
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-05-08 10:46:36 +0200

    feat: add table

--- src/App.jsx
+++ src/App.jsx
@@ -11,36 +11,43 @@ function App() {
         goPrevPage
     } = usePeople();
 
-
-
-
-    if (loading) {
-        return <p>Loading...</p>
-    }
-
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Star Wars Characters</h1>
             </header>
             <main>
-                <ul>
-                    {people?.map((person) => (
-                        <li key={person.name}>{person.name}</li>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Birth Year</th>
+                        <th>Gender</th>
+                        <th>Eye Color</th>
+                        <th>Hair Color</th>
+                        <th>Height</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {people?.map((person, key) => (
+                        <tr key={`${person.name}-${key}`}>
+                            <td>{person.name}</td>
+                            <td>{person.birth_year}</td>
+                            <td>{person.gender}</td>
+                            <td>{person.eye_color}</td>
+                            <td>{person.hair_color}</td>
+                            <td>{person.height}</td>
+                        </tr>
                     ))}
-                </ul>
-
+                    </tbody>
+                </table>
                 <div>
-                    {prevPage && (
-                        <button onClick={goPrevPage}>
-                            Previous
-                        </button>
-                    )}
-                    {nextPage && (
-                        <button onClick={goNextPage}>
-                            Next
-                        </button>
-                    )}
+                    <button onClick={goPrevPage} disabled={loading || !prevPage}>
+                        Previous
+                    </button>
+                    <button onClick={goNextPage} disabled={loading || !nextPage}>
+                        Next
+                    </button>
                 </div>
             </main>
         </div>);
