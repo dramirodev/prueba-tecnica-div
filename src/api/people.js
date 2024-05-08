d73b7c8bdbfc6a84038653bcb5409c6af6c6679c
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2024-05-08 12:19:02 +0200

    feat: finish cache feature

--- src/api/people.js
+++ src/api/people.js
@@ -1,7 +1,6 @@
-export const PEOPLE_API_URL = 'https://swapi.dev/api/people/'
-
+export const PEOPLE_API_URL = "https://swapi.dev/api/people/";
 
 export const getPeoplePaginated = async (page) => {
-    const response = await fetch(page);
-    return await response.json()
-}
+  const response = await fetch(page);
+  return await response.json();
+};
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,28 +1,28 @@
-export function Table({people = []}) {
-    return (
-        <table>
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Birth Year</th>
-                <th>Gender</th>
-                <th>Eye Color</th>
-                <th>Hair Color</th>
-                <th>Height</th>
-            </tr>
-            </thead>
-            <tbody>
-            {people?.map((person, key) => (
-                <tr key={`${person.name}-${key}`}>
-                    <td>{person.name}</td>
-                    <td>{person.birth_year}</td>
-                    <td>{person.gender}</td>
-                    <td>{person.eye_color}</td>
-                    <td>{person.hair_color}</td>
-                    <td>{person.height}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    )
+export function Table({ people = [] }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Birth Year</th>
+          <th>Gender</th>
+          <th>Eye Color</th>
+          <th>Hair Color</th>
+          <th>Height</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people?.map((person, key) => (
+          <tr key={`${person.name}-${key}`}>
+            <td>{person.name}</td>
+            <td>{person.birth_year}</td>
+            <td>{person.gender}</td>
+            <td>{person.eye_color}</td>
+            <td>{person.hair_color}</td>
+            <td>{person.height}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
